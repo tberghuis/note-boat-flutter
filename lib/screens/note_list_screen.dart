c2f2e058ef9c017a000f5ede861f8a19d67e8608
Thomas Berghuis <thomas.berghuis@gmail.com>
2020-06-17 11:47:43 +1000

    .

---
+++ lib/screens/note_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
